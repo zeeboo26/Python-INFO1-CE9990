350e036f54223140504586b4d9e45eab6ca656aa
zeldasalfati <zeldasalfati@users.noreply.github.com>
2017-07-13 09:46:35 -0400

    Update poèmeChansonDautomne.py

--- poèmeChansonDautomne.py
+++ poèmeChansonDautomne.py
@@ -1,5 +1,5 @@
 """
-poème.py
+poèmeChansonDautomne.py
 
 This program inputs all the lines of a text file from the web and does something not very interesting with them 
 (i.e sort lines in order of decreasing length)
@@ -12,22 +12,22 @@ import urllib.request
 url = "https://raw.githubusercontent.com/zeldasalfati/Python-INFO1-CE9990/master/ChansonDautomne"
 
 try:                                                                      
-    filename = urllib.request.urlopen(url) # we were successful
+    inputFile = urllib.request.urlopen(url) # we were successful
 except urllib.error.URLError as error: 
     print("The url", url, "does not exist") # we weren't successful
     sys.exit(1)
-
+    
 # read the filename
-lines = filename.readlines()
+lines = inputFile.readlines()
+inputFile.close()
 
-#sort lines in order of decreasing length
-lines.sort(reverse = True) 
-
-# use decode to onverts a sequence of bytes into a string of characters. 
+ # use decode to onverts a sequence of bytes into a string of characters. 
 for line in lines:
     # s = line.decode("utf-8")
     # print(s, end = "")
-    print(line.decode("utf-8"), end = "")
-    
-lines.close()
+    print(line.decode("utf-8"), end = "")   
+
+#sort lines in order of decreasing length
+lines.sort(key = len, reverse = True) 
+
 sys.exit(0)
