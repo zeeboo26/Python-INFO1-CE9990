f863a161720bdd1cb898121c120eb82990aae168
zeldasalfati <zeldasalfati@users.noreply.github.com>
2017-07-29 16:18:27 -0400

    Create json.py

---
+++ json.py
@@ -0,0 +1,41 @@
+"""
+json.py
+
+This programs downloads information about date and time from a website into
+JSON format.
+See:  http://date.jsontest.com
+"""
+
+import sys
+import urllib.request
+import json
+
+url = "http://date.jsontest.com"
+
+try:
+    infile = urllib.request.urlopen(url)
+except urllib.error.URLError as error:
+    print("urllib.error.URLError", error)
+    sys.exit(1)
+
+sequenceOfBytes = infile.read()         #Read the entire input file.
+infile.close()
+
+try:
+    s = sequenceOfBytes.decode("utf-8") #s is a string.
+except UnicodeError as unicodeError:
+    print(unicodeError)
+    sys.exit(1)
+
+print(s)
+print()
+
+try:
+    dictionary = json.loads(s)          #dictionary is a dictionary.
+except json.JSONDecodeError as jSONDecodeError:
+    print(jSONDecodeError)
+    sys.exit(1)
+
+print(dictionary)
+print()
+sys.exit(0)
